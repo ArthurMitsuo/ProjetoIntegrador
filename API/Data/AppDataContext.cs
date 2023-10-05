8233def9802517322c78fe157a651a410a6ed58c
Arthur Mitsuo <77021980+ArthurMitsuo@users.noreply.github.com>
2023-10-04 23:31:07 -0300

    commit inicial com o projeto Web API criado e primeiras modificações feitas. Já configurado o EntityFramework, em teoria

---
+++ API/Data/AppDataContext.cs
@@ -0,0 +1,16 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class AppDataContext : DbContext
+{
+     public AppDataContext(DbContextOptions<AppDataContext> options) : base(options)
+    {
+
+    }
+
+    //Classes que vão se tornar tabelas no banco de dados
+    //public DbSet<Produto> Produtos { get; set; }
+    //public DbSet<Categoria> Categorias { get; set; }
+}
